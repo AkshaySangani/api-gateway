6ba42e5948d30b83106ab0f08a161fd300222d38
Akshay <akshay10sangani@gmail.com>
2025-03-14 00:48:31 +0530

    channel service added

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,8 +2,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 
-const { PORT, AUTH_SERVICE_URL, PRODUCT_SERVICE_URL } = process.env
+const { PORT, AUTH_SERVICE_URL, PRODUCT_SERVICE_URL, CHANNEL_SERVICE_URL } = process.env
 
 const AUTH_URL = AUTH_SERVICE_URL || ""
 const PRODUCT_URL = PRODUCT_SERVICE_URL || ""
-export { PORT, AUTH_URL, PRODUCT_URL }
+const CHANNEL_URL = CHANNEL_SERVICE_URL || ""
+export { PORT, AUTH_URL, PRODUCT_URL, CHANNEL_URL }
